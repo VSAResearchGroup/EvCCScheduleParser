d75149d33966879d5ebce2caaa1afeb75ed264ec
Nathan Harger <nathan.j.harger@gmail.com>
2017-06-29 14:37:49 -0700

    Added writing class info to exel file

--- evccScheduleParser.py
+++ evccScheduleParser.py
@@ -6,6 +6,7 @@
 
 from lxml import html as a
 import numpy as np
+import xlwt
 
 def print_row(row):
     for i in row:
@@ -13,39 +14,68 @@ def print_row(row):
         break
     print "-------------------"
 
-#
-page = file("Summer_Class Schedule __ EvCC.html")
+def create_exel(quarters, schedule):
+    wb = xlwt.Workbook()
+    for curr_quarter in range(len(quarters)):
+        ws = wb.add_sheet(quarters[curr_quarter] + " Schedule",cell_overwrite_ok=True)
+        labels = ["Course", "Desc", "Section", "Credits", "Capacity", "Enrolled", "Time","Day","Location"]
+        for curr_label in range(len(labels)):
+            ws.write(0,curr_label, labels[curr_label],style=xlwt.Style.easyxf(strg_to_parse="font: bold on;"))
 
-tree = a.fromstring(page.read())
+        for i in range(np.size(schedule[curr_quarter],0)):
+            for j in range(np.size(schedule[curr_quarter],1)):
 
-page.close()
-
-course_title = tree.xpath('//*[contains(@id,"div")]/td/table/tr/td/h3/text()')
-desc = tree.xpath('//*[contains(@id,"div")]/td/table/tr/td/p[1]')
-
-# filter out empty descriptions
-desc_result = []
-desc = [desc_result.append(x.text) if x.text else '' for x in desc ]
-
-section = tree.xpath('//*[contains(@id,"div")]/td/table/tr/td/table/tr[2]/td[2]/text()[normalize-space(.) != .]')
-
-credits = tree.xpath('//*[contains(@id,"div")]/td/table/tr/td/table/tr[2]/td[3]/text()')
-
-capacity = tree.xpath('//*[contains(@id,"div")]/td/table/tr/td/table/tr[4]/td[1]/text()')
+                # scheduleschedule[curr_quarter][i,j] is used instead of schedule[curr_quarter,i,j] because
+                # schedule is a 2d java array of 2d numpy arrays
+                ws.write(1+i,j, schedule[curr_quarter][i,j])
+    wb.save("test.xls")
 
 
-enrolled = tree.xpath('//*[contains(@id,"div")]/td/table/tr/td/table/tr[4]/td[2]/text()')
+def create_schedule(target_qrt):
+    page = file(target_qrt + "_Class Schedule_EvCC.html")
 
-start_end_time = tree.xpath('//*[contains(@id,"div")]/td/table/tr/td/table/tr[6]/td[1]/text()')
+    tree = a.fromstring(page.read())
 
-days = tree.xpath('//*[contains(@id,"div")]/td/table/tr/td/table/tr[6]/td[2]')
+    page.close()
 
-# remove empty days
-days = [x.text if x.text else '' for x in days]
+    course_title = tree.xpath('//*[contains(@id,"div")]/td/table/tr/td/h3/text()')
+    desc = tree.xpath('//*[contains(@id,"div")]/td/table/tr/td/p[1]')
 
-location = tree.xpath('//*[contains(@id,"div")]/td/table/tr/td/table/tr[6]/td[3]/text()')
-table = [course_title,desc_result,section,credits,capacity,enrolled,start_end_time,days,location]
+    # filter out empty descriptions
+    desc_result = []
+    desc = [desc_result.append(x.text) if x.text else '' for x in desc ]
 
-schedule = np.empty((len(section),len(table)),dtype=object)
-for i in range(len(table)):
-    schedule[:,i] = table[i]
+    section = tree.xpath('//*[contains(@id,"div")]/td/table/tr/td/table/tr[2]/td[2]/text()[normalize-space(.) != .]')
+
+    credits = tree.xpath('//*[contains(@id,"div")]/td/table/tr/td/table/tr[2]/td[3]/text()')
+
+    capacity = tree.xpath('//*[contains(@id,"div")]/td/table/tr/td/table/tr[4]/td[1]/text()')
+
+
+    enrolled = tree.xpath('//*[contains(@id,"div")]/td/table/tr/td/table/tr[4]/td[2]/text()')
+
+    start_end_time = tree.xpath('//*[contains(@id,"div")]/td/table/tr/td/table/tr[6]/td[1]/text()')
+
+    days = tree.xpath('//*[contains(@id,"div")]/td/table/tr/td/table/tr[6]/td[2]')
+
+    # remove empty days
+    days = [x.text if x.text else '' for x in days]
+
+    location = tree.xpath('//*[contains(@id,"div")]/td/table/tr/td/table/tr[6]/td[3]/text()')
+    table = [course_title,desc_result,section,credits,capacity,enrolled,start_end_time,days,location]
+
+    schedule = np.empty((len(section),len(table)),dtype=object)
+    for i in range(len(table)):
+        schedule[:,i] = table[i]
+    return schedule
+
+if __name__ == "__main__":
+    #
+    quarters = ["Summer", "Fall", "Spring"]
+    schedules = []
+    for i in quarters:
+        schedules.append(create_schedule(i))
+
+    create_exel(quarters, schedules)
+
+
