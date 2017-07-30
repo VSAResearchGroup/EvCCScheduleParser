f1ee1718bbb3f35baf7a676d09c10398a7b90295
nathan <nathan.j.harger@gmail.com>
2017-07-29 23:48:42 -0700

    Added prereqs. Two combined csv are generated rather than having the csv divided by quarters

--- evccScheduleParser.py
+++ evccScheduleParser.py
@@ -15,35 +15,22 @@ def print_row(row):
     print "-------------------"
 
 
-def create_cvs(quarter, schedule, fieldnames):
-   # wb = xlwt.Workbook()
-   #  for curr_quarter in range(len(quarters)):
-   #      ws = wb.add_sheet(quarters[curr_quarter] + " Schedule",cell_overwrite_ok=True)
-   #      labels = ["Course", "Desc", "Section", "Credits", "Start Time","End Time","Day","Location"]
-   #      for curr_label in range(len(labels)):
-   #          ws.write(0,curr_label, labels[curr_label],style=xlwt.Style.easyxf(strg_to_parse="font: bold on;"))
-   #
-   #      for i in range(np.size(schedule[curr_quarter],0)):
-   #          print str(i) +"/ " + str(np.size(schedule[curr_quarter],0))
-   #
-   #          for j in range(np.size(schedule[curr_quarter],1)):
-   #              ws.write(1+i,j, schedule[curr_quarter][i][j])
-   #  wb.save("test.xls")
-
+def create_cvs(filename, schedules, fieldnames):
   # source for use of zip function with dictionary init:
   # https://stackoverflow.com/questions/209840/map-two-lists-into-a-dictionary-in-python
 
     # encode all unicode characters into ascii in the description
     if "Description" in fieldnames:
-        for i in schedule:
-            i[5] = i[5].encode('ascii', 'ignore')
+        for i in range(len(schedules)):
+            for j in schedules[i]:
+                j[5] = j[5].encode('ascii', 'ignore')
 
-    with open(quarter+".csv", 'w') as csvfile:
+    with open(filename+".csv", 'w') as csvfile:
         writer = csv.DictWriter(csvfile,fieldnames=fieldnames)
         writer.writeheader()
-        for i in schedule:
-            #print i
-            writer.writerow(dict(zip(fieldnames,i)))
+        for i in range(len(schedules)):
+            for j in schedules[i]:
+                writer.writerow(dict(zip(fieldnames,j)))
 
 def convert_to_numerical_time(start, end):
     for i in range(len(start)):
@@ -100,6 +87,7 @@ def parse_title(titles):
             titles_r.append(" ".join(core_info[2:len(core_info)]))
 
     return [numbers,titles_r]
+
 def parse_days_from_string(days):
     "MTWTh"
 
@@ -182,10 +170,45 @@ def create_schedule(target_qrt):
     desc = tree.xpath('//*[contains(@id,"div")]/td/table/tr/td/p[1]')
 
     # filter out empty descriptions
-
     # Create a list of descriptions containing stripped version of the data read from the p tag. Replace the misformmated
     # "No Description Available" to a stripped representaion.
     desc = [x.text.strip() if "No Description Available" not in x.text else "No Description Available" for x in desc ]
+
+    raw_prereq = tree.xpath('//*[contains(@id,"div")]/td/table/tr/td')
+    prereq = []
+    for i in range(len(raw_prereq)):
+        c = raw_prereq[i].getchildren()
+        count  = 0
+
+
+        p_tags = [x  for x in c if a._element_name(x) == 'p']
+
+        print len(p_tags)
+        if len(p_tags) == 0:
+            continue
+        elif len(p_tags) == 1 :
+            prereq.append("")
+
+        else:
+            current_string = str(p_tags[1].text_content().strip().encode('ascii', 'ignore'))
+            if "Prereq:" not in current_string:
+                prereq.append("")
+            else:
+                prereq.append(current_string.split("Prereq: ")[1])
+
+
+
+    # hack to remove floating \n in list.
+    #raw_prereq = [x if x.strip() != "" else "" for x in raw_prereq  ]
+    #prereq = []
+
+
+   # for i in range(len(raw_prereq)):
+    #    if i % 2 == 0:
+     #       continue
+      #  else:
+       #     prereq.append(raw_prereq[i])
+
 
     section = tree.xpath('//*[contains(@id,"div")]/td/table/tr/td/table/tr[2]/td[2]/text()[normalize-space(.) != .]')
 
@@ -196,6 +219,7 @@ def create_schedule(target_qrt):
     enrolled = tree.xpath('//*[contains(@id,"div")]/td/table/tr/td/table/tr[4]/td[2]/text()')
 
     start_end_time = tree.xpath('//*[contains(@id,"div")]/td/table/tr/td/table/tr[6]/td[1]/text()')
+
     start_time = [x.split("-")[0] if '-' in x  else '' for x in start_end_time]
     end_time = [x.split("-")[1] if '-' in x else '' for x in start_end_time]
 
@@ -206,42 +230,36 @@ def create_schedule(target_qrt):
     days = [x.text if x.text else '' for x in days]
 
     location = tree.xpath('//*[contains(@id,"div")]/td/table/tr/td/table/tr[6]/td[3]/text()')
-    #add duplicates per days
 
     day_list = parse_days_from_string(days)
-    #print day_list
-    #print course_title
 
     [course_numbers,course_titles] = parse_title(course_title)
     scheduleCourse = []
     scheduleCourseTime = []
-
+    print len(prereq)
     for course in range(len(course_numbers)):
         print course_numbers[course]
-        scheduleCourse.append([course_numbers[course] ,section[course], course_titles[course], -1, credits[course], desc[course]])
+        scheduleCourse.append([course_numbers[course] ,section[course], course_titles[course], credits[course], credits[course], desc[course],prereq[course]])
         scheduleDays = day_list[course]
         for i in range(len(scheduleDays)):
-
-            #schedule.append([course_title[i],desc_result[i],section[i],credits[i],start_time[i],end_time[i],day_list[i][j],location[i]])
-            scheduleCourseTime.append([course_numbers[course],section[course],start_time[course],end_time[course],scheduleDays[i] ,get_qtr_id(target_qrt), -1,-1])
-
-    #for i in range(len(table)):
-     #   schedule[:,i] = table[i]
+            scheduleCourseTime.append([course_numbers[course],start_time[course],end_time[course],scheduleDays[i] ,get_qtr_id(target_qrt), 1,-1])
 
 
-    #schedule = [x * for x in schedule]
     return [scheduleCourse,scheduleCourseTime]
 
 if __name__ == "__main__":
     #
     quarters = ["Summer", "Fall", "Spring"]
 
+    scheduleCourses = []
+    scheduleCourseTimes = []
     for i in range(len(quarters)):
         scheduleCourse, scheduleCourseTime = create_schedule(quarters[i])
-        print "\n" + quarters[i]
+        scheduleCourses.append(scheduleCourse)
+        scheduleCourseTimes.append(scheduleCourseTime)
 
-        create_cvs("scheduleCourse"+quarters[i] , scheduleCourse, ["CourseNumber", "Section", "Title","MinCredit", "MaxCredit", "Description"])
-        create_cvs("scheduleCourseTime"+quarters[i] , scheduleCourseTime, ["CourseNumber","Section", "StartTimeID","EndTimeID","DayID" ,"QuarterID", "Year","Status"])
+    create_cvs("scheduleCourse" , scheduleCourses, ["CourseNumber", "Section", "Title","MinCredit", "MaxCredit", "Description", "PreRequisites"])
+    create_cvs("scheduleCourseTime" , scheduleCourseTimes, ["CourseNumber", "StartTimeID","EndTimeID","DayID" ,"QuarterID", "Year","Status"])
 
     #print schedules
 
